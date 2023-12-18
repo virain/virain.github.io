e0f4f70e4ba99d3d8a0272b553171034ec310421
virain <xu_junkai@hotmail.com>
2023-12-18 17:30:10 +0800

    Quartz sync: Dec 18, 2023, 5:30 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "virain.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
