f66f6c04ba45f1c411fd04486f9d49a17ca34e7c
virain <xu_junkai@hotmail.com>
2023-12-20 10:42:07 +0800

    Quartz sync: Dec 20, 2023, 10:42 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -28,9 +28,9 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    // Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
+    // Component.Backlinks(),
   ],
 }
 
